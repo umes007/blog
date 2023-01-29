643e88a386c876002d6826a45090130031bf7cee
Umesh Hyaunmikha <uh7umesh@gmail.com>
2023-01-29 10:27:37 +0545

    create, update, delete, articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,11 +2,45 @@ class ArticlesController < ApplicationController
 
   def index
     @count = Article.count
-    @articles = Article.limit(30)
+    @articles = Article.order(created_at: :desc).limit(30)
   end
 
   def show
     @article = Article.find(params[:id])
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to "/articles"
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(title: params[:article][:title], body: params[:article][:body])
+
+    if @article.save
+      redirect_to "/articles/#{@article.id}"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find params[:id]
+
+    if @article.update(title: params[:title], body: params[:body])
+      redirect_to "/articles/#{@article.id}"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   get "/articles", to: "articles#index"
+  get "/articles/new", to: "articles#new"
+  post "/articles", to: "articles#create"
   get "/articles/:id", to: "articles#show"
+  get "/articles/:id/edit", to: "articles#edit"
+  patch "/articles/:id", to: "articles#update"
+  delete "/articles/:id", to: "articles#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
